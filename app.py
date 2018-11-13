d3d085dfc57344e2d80f8d4cd53a99ba71113f3e
kibin <rlqls505@naver.com>
2018-11-13 17:54:02 +0900

    gibin
    2018.11.13.17.53

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('/accounts/login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('/accounts/signup.html')
+
+@app.route('/add_project')
+def add_project():
+    return render_template('/project/add_project')
+
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(12) # for session
     app.run(debug=True, host='0.0.0.0') # https://flask-docs-kr.readthedocs.io/ko/latest/quickstart.html / public ip acess 가능 
